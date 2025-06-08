14af654852f4fd6b5cc0b8c6da5a01d9c23b2233
dropsik15 <bborkowskajulia@gmail.com>
2025-06-09 00:51:18 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  base: '/technologie-webowe/',
-  build: {
+    base: '/technologie-webowe/', // needed for GitHub Pages!
+    build: {
         outDir: 'dist',
-   },
+    },
+    plugins: [
+        tailwindcss(),
+    ],
 })
